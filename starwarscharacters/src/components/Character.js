46deaaae7bf09f6a481331a9ca32b07dacd904eb
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-04-16 08:25:11 -0600

    Built skeleton, but not working yet

---
+++ starwarscharacters/src/components/Character.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledName = styled.h2 `
+    &: hover {
+        color: white;
+        text-shadow: none;
+    }
+`
+
+const StyledP = styled.p `
+    display: flex;
+    justify-content: center;
+    &: hover {
+        color: black;
+        text-shadow: none;
+        
+    }
+`
+
+
+
+export default function Character ({ character }) {
+    return (
+        <div>
+            <StyledName>Name: {character.name}</StyledName>
+            <StyledP>Birth Year: {character.birth_year}</StyledP>
+            <StyledP>Height: {character.height}</StyledP>
+            <StyledP>Mass: {character.mass}</StyledP>
+        </div>
+    )
+}
